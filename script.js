4574ad833cd3a7dd2d321b78628954b62ee0ce8a
Labrot <sarah.lientz@gmail.com config --global user.email>
2022-01-10 13:28:20 -0500

    updated screenshots

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 
-
-
-document.getElementsByClassName("title is-4").addEventListener("mouseenter", function() {
-    document.getElementsByClassName("title is-4").style.color = "blue";
-  })
-  document.getElementsByClassName("title is-4").addEventListener("mouseleave", function() {
-    document.getElementsByClassName("title is-4").style.color = "hsl(360, 91%, 36%)";
-  })
+$(document).ready(function(){
+    $('#imgCard2').hover(function() {
+        $("#imgCard2").addClass('transition');
+    
+    }, function() {
+        $("#imgCard2").removeClass('transition');
+    });
+});
