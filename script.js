939763b358780e9e923dfbefbe2ab2074743636a
Labrot <sarah.lientz@gmail.com config --global user.email>
2022-01-10 12:23:48 -0500

    added hover to images in css

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+
+document.getElementsByClassName("title is-4").addEventListener("mouseenter", function() {
+    document.getElementsByClassName("title is-4").style.color = "blue";
+  })
+  document.getElementsByClassName("title is-4").addEventListener("mouseleave", function() {
+    document.getElementsByClassName("title is-4").style.color = "hsl(360, 91%, 36%)";
+  })
